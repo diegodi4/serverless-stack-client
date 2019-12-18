8215283fa34862de77d5074fec5839fa5ce31b0e
diegodi4 <diquattro@gmail.com>
2019-12-18 16:47:00 +0000

    Setting up our React app

---
+++ src/containers/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-uploads-di4"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://twi738n50g.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_26wWlVBsp",
+    APP_CLIENT_ID: "1egl99c13fnp7v0m7mut2kfq23",
+    IDENTITY_POOL_ID: "eu-central-1:28cc73c3-3d6d-43c5-9659-46e830a47d0a"
+  }
+};
